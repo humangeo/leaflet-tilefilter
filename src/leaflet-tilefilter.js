ea22669ab8661a54f63046a18d702b33fc5230eb
Scott Fairgrieve <scottfairgrieve@MacBook-Pro-3.local>
2015-12-02 19:56:09 -0500

    Fix for issue with options not being defined

--- src/leaflet-tilefilter.js
+++ src/leaflet-tilefilter.js
@@ -649,7 +649,7 @@ L.ChannelFilters.Matrix = L.ChannelFilter.extend({
     options: {
         matrix: [ 0.393, 0.769, 0.189, 0.349, 0.686, 0.168, 0.272, 0.534, 0.131 ]
     },
-    initialize: function () {
+    initialize: function (options) {
         L.ChannelFilter.prototype.initialize.call(this, options);
     },
     updateChannels: function(channels) {
